df2446f27f7bb2dc1d784a5b4376ed2d4dd6d303
Brdantas <114081814+Brdantas@users.noreply.github.com>
2023-11-09 09:12:54 -0300

    Update cargos.sql

--- cargos.sql
+++ cargos.sql
@@ -11,11 +11,11 @@ CREATE TABLE funcionarios (
 
 -- Inserção de dados na tabela funcionarios
 INSERT INTO funcionarios (id, nome, cargo, departamento) VALUES
-(1, 'Nome Funcionario 1', 'Cargo 1', 'Administrativo'),
-(2, 'Nome Funcionario 2', 'Cargo 2', 'Vendas'),
+(1, 'Nome Funcionario 1', 'Cargo 1', 'Desenvolvedor'),
+(2, 'Nome Funcionario 2', 'Cargo 2', 'Logistica'),
 (3, 'Nome Funcionario 3', 'Cargo 3', 'Administrativo'),
-(4, 'Nome Funcionario 4', 'Cargo 4', 'RH'),
-(5, 'Nome Funcionario 5', 'Cargo 5', 'Administrativo');
+(4, 'Nome Funcionario 4', 'Cargo 4', 'Bancario'),
+(5, 'Nome Funcionario 5', 'Cargo 5', 'Adiministrador');
 
 
 -- Criação da tabela departamentos
@@ -26,9 +26,9 @@ CREATE TABLE departamentos (
 
 -- Inserção de dados na tabela departamentos
 INSERT INTO departamentos (id, nome_departamento) VALUES
-(1, 'Administrativo'),
-(2, 'Vendas'),
-(3, 'RH');
+(1, 'Desenvolvedor'),
+(2, 'Logistica'),
+(3, 'Administrativo');
 
 CREATE VIEW vw_funcionarios_departamento AS
 SELECT
@@ -37,7 +37,7 @@ SELECT
 FROM
     funcionarios
 WHERE
-    departamento = 'Administrativo';
+    departamento = 'Administrador';
     
     SELECT * FROM vw_funcionarios_departamento;
 
