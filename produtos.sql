6e414d22886c427f5224631ad034f76e8e55ca1e
Brdantas <114081814+Brdantas@users.noreply.github.com>
2023-11-09 09:28:41 -0300

    Update produtos.sql

--- produtos.sql
+++ produtos.sql
@@ -5,14 +5,14 @@ CREATE TABLE produtos (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome_do_produto VARCHAR(255) NOT NULL,
     data_validade DATE NOT NULL,
-    preco DECIMAL(10, 2) NOT NULL
+    preco DECIMAL(10, 5) NOT NULL
 );
 
 
 INSERT INTO produtos (nome_do_produto, data_validade, preco)
-VALUES ('Produto A', '2023-12-31', 50.99),
-       ('Produto B', '2023-11-15', 75.50),
-       ('Produto C', '2023-10-05', 120.00);
+VALUES ('Produto A', '2023-07-22', 50.99),
+       ('Produto B', '2023-02-14', 75.50),
+       ('Produto C', '2023-11-07', 120.00);
        
 
 DELIMITER //
@@ -22,8 +22,8 @@ BEFORE INSERT ON produtos
 FOR EACH ROW
 BEGIN
     IF NEW.data_validade < CURDATE() THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Erro: Produto com data de validade vencida não pode ser inserido';
+        SIGNAL SQLSTATE '75000'
+        SET MESSAGE_TEXT = 'Erro:Produto fora da validade não pode ser vendido';
     END IF;
 END;
 //
